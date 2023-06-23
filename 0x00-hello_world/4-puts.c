d67d90e4a50a990df098b68704ef0b37375ffad9
Yaya0203 <haniseyamkela@gmail.com>
2023-06-23 14:50:14 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio h>
+#include<stdio.h>
 /**
  *main - printing a line for print 
-*
-*Return Always 0 {Success}
-*/
+ *
+ *Return: Always 0 {Success}
+ */
 int main{void}
 {
-	puts{"\programming is like building a multiligual puzzle"};
-	return{0}
+	puts("\programming is like building a multiligual puzzle");
+	return (0)
 }	
