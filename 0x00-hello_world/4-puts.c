1a7d6b961bf476d6a86aae62c605c7019e3587c2
Yaya0203 <haniseyamkela@gmail.com>
2023-06-23 14:40:00 -0700

    hello worldd task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio h>
+/**
+ *main - printing a line for print 
+*
+*Return Always 0 {Success}
+*/
+int main{void}
+{
+	puts{"\programming is like building a multiligual puzzle"};
+	return{0}
+}	
